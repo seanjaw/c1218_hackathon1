0a4f3c32f4697f57a8b8760731a2bff408f0c85e
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-16 17:23:27 -0800

    Setup adding players

--- team4/js/main.js
+++ team4/js/main.js
@@ -3,5 +3,11 @@ let game = null;
 
 function initApp(){
     game = new Game();
-
 }
+
+class selectNumberOfPlayers {
+    constructor(numberOfPlayers){
+        
+    }
+}
+
--- team4/js/player.js
+++ team4/js/player.js
@@ -1,7 +1,10 @@
 class Player{
     constructor( square ){
         this.square = square;
+        this.createPlayer = null;
+        this.playerColor = ["red", "blue", "green", "yellow"];
     }
+
     rolldice(){
         let rollArray= [];
         for (let die=0; die<2; die++ ){
@@ -11,6 +14,7 @@ class Player{
         console.log(rollArray);
         return rollArray;
     }
+
     move( amount ){
         for (let i = 0; i < amount; i++) {
             this.square = this.square.next;
@@ -18,4 +22,23 @@ class Player{
         console.log('After move: ', this.square.title);
     }
 
+    createNewPlayer(){ 
+        if ($("h1").length > 3){
+            console.error("Can only have 4 players!")
+        } else { //Creating new player with accordion settings
+        let numOfPlayers = $("h1").length + 1;
+        
+        this.createPlayer = $("<h1>")
+            .css("background-color", this.playerColor[$("h1").length])
+            .text("Player" + numOfPlayers);
+        // this.createNewPlayer.append(spanElem);
+        
+        $("#accordion").append(this.createPlayer);
+        }
+    }
+
+    setPlayerList(){
+        $("#accordion").accordion();
+    }
 }
+
