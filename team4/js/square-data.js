f8fed9ef86647bb2bef5c183fbbfd022516f6516
seanjaw <sjaw94@gmail.com>
2019-01-20 21:19:16 -0800

    save work

--- team4/js/square-data.js
+++ team4/js/square-data.js
@@ -17,7 +17,7 @@ street	Sailor Mercury	140	10;20;50;150;450;625;750	pink	bbacf47ead84bd053b6ae390
 utility	Electric Company	150			
 street	Chibiusa	140	10;20;50;150;450;625;750	pink	Little-Princess-Serenity-sailor-mini-moon-rini-9658735-200-239.gif
 street	Sailor Venus	160	12;24;60;180;500;700;900	pink	sailor%20moon%20pic18.png
-utility	Water Works	150		
+utility	Water Works 150		
 street	Sailor Mars	180	14;28;70;200;550;750;950	orange	tumblr_mkph7z6fmA1r49apoo1_500.png
 community-chest	LOVE				
 street	Sailor Jupiter	180	14;28;70;200;550;750;950	orange	sailor%20moon%20pic15.png
